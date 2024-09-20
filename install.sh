2b28400e93978995f29f8d708e2258e524eaa512
0XP0 <anonymous@Anonymous.com>
2024-09-20 08:08:09 +0800

    shell

--- install.sh
+++ install.sh
@@ -8,16 +8,20 @@ target=$(uname -m)
 # path to the gaianet base directory
 gaianet_base_dir="$HOME/gaianet"
 
-
-
 check_curl() {
     curl --retry 3 --progress-bar -L "$1" -o "$2"
 
     if [ $? -ne 0 ]; then
-        error "    * Failed to download $1"
+        echo "    * Failed to download $1" >&2
         exit 1
     fi
 }
 
+# Download files
 check_curl https://github.com/YuanTony/chemistry-assistant/raw/main/rag-embeddings/create_embeddings.wasm create_embeddings.wasm 
-check_curl https://github.com/0xP0/toqdrant/releases/latest/download/toqdrant.py toqdrant.py                                                                                                                   
+check_curl https://github.com/0xP0/toqdrant/releases/latest/download/toqdrant.sh $gaianet_base_dir/bin/toqdrant
+
+# Make toqdrant executable
+chmod +x $gaianet_base_dir/bin/toqdrant
+
+echo "toqdrant has been downloaded and made executable."
